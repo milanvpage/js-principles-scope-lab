78c0a75af72c722f20f1b1a9d6da0357b81d6bd8
Milan <milanvpage@gmail.com>
2021-09-13 00:03:32 -0700

    commit tree 843b940da0ae3dadab7905ab30b83db8f42caed5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "Milan";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'tim'
+
+}
+
+
